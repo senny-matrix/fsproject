998eedb608869f418d5bed31cd6ba5eaef97da9f
Senny Aaron <arumeru@gmail.com>
2022-03-25 15:46:35 +0300

    After correcting caps

--- change_domain_name.py
+++ change_domain_name.py
@@ -2,7 +2,7 @@
 
 import subprocess
 
-domain_name = input("write the domain name (e.g schoolname.pef.local): ")
+domain_name = input("Write the domain name (e.g schoolname.pef.local): ")
 
 subprocess.call(["echo", domain_name, "> /etc/hostname"])
 subprocess.call(["reboot"])
